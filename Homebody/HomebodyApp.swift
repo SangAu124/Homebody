c86fd1d2b1cf01a45699ad9a16e2c0c5d7bc0260
김상은 <ksemms20@dgsw.hs.kr>
2023-03-11 14:19:51 +0900

    🎉 :: Start

---
+++ Homebody/HomebodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomebodyApp.swift
+//  Homebody
+//
+//  Created by 김상금 on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct HomebodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
