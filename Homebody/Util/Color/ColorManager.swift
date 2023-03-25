3783eabd929262bae311fc0a7cbf7af36c031c1e
김상은 <ksemms20@dgsw.hs.kr>
2023-03-25 18:02:51 +0900

    💄 :: Color 설정

---
+++ Homebody/Util/Color/ColorManager.swift
@@ -0,0 +1,20 @@
+//
+//  ColorManager.swift
+//  Homebody
+//
+//  Created by 김상금 on 2023/03/11.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorManager {
+    static let Primary_Orange = Color("Primary_Orange")
+    static let Primary_Yellow = Color("Primary_Yellow")
+    static let Primary_Beige = Color("Primary_Beige")
+    static let Primary_Brown = Color("Primary_Brown")
+    static let Gray1 = Color("Gray1")
+    static let Gray2 = Color("Gray2")
+    static let Gray3 = Color("Gray3")
+    static let Gray4 = Color("Gray4")
+}
